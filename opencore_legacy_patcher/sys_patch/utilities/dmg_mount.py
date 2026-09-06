Medelcartelinc/OpenCore-Legacy-Patcher-T2

commit 43a8ac11df9ca5b983b13b1b8e0a6aaae89e6487

sys_patch/dmg_mount: remove hardcoded password and enable elevated mount retry

opencore_legacy_patcher/sys_patch/utilities/dmg_mount.py
@@ -58,9 +58,6 @@ class PatcherSupportPkgMount:
             dmg_path,
             Path(self.constants.payload_path / "Universal-Binaries"),
             shadow_path=Path(self.constants.payload_path / "Universal-Binaries_overlay"),
-            password="password",
-            # Fixed, known-correct password: "Authentication error" here can only mean
-            # the privilege gate/quarantine issue, never a wrong password (see mount_dmg)
             retry_on_auth_error=True
         )
 

commit 64773713bf6e4d7234cb34189facd5d2371edb01

sys_patch/dmg_mount: restore DMG password - fixes Universal-Binaries mount failure
The hardcoded passphrase 'password' is required by hdiutil to decrypt
Universal-Binaries.dmg (PatcherSupportPkg convention). Commit 43a8ac1
mistakenly removed it, causing all root patch attempts to fail with
'hdiutil: attach failed - Permission denied' (or Authentication error)
since hdiutil rejects an empty passphrase for an encrypted image.

Fixes: https://github.com/Medelcartelinc/OpenCore-Legacy-Patcher-T2/issues/5

opencore_legacy_patcher/sys_patch/utilities/dmg_mount.py
@@ -58,6 +58,7 @@ class PatcherSupportPkgMount:
             dmg_path,
             Path(self.constants.payload_path / "Universal-Binaries"),
             shadow_path=Path(self.constants.payload_path / "Universal-Binaries_overlay"),
+            password="password",
             retry_on_auth_error=True
         )
 