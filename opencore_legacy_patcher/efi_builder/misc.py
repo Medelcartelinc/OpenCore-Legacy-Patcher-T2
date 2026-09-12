55ba60d669cb11b20922bb603434c55dffd4c2a7
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-09-12 06:50:37 +0300

    Revert "fix kernel panic on non T2 macs"
    This reverts commit da3526eb3446cec44c459ded91302c73692e4a52.

--- opencore_legacy_patcher/efi_builder/misc.py
+++ opencore_legacy_patcher/efi_builder/misc.py
@@ -119,14 +119,9 @@ class BuildMiscellaneous:
 
         if block_args:
             logging.info(f"- Setting RestrictEvents block arguments: {block_args}")
-            if self._is_t2_mac():
-                support.BuildSupport(self.model, self.constants, self.config).enable_kext(
-                    "RestrictEvents.kext", self.constants.restrictevents_t2_version, self.constants.restrictevents_t2_path
-                )
-            else:
-                support.BuildSupport(self.model, self.constants, self.config).enable_kext(
-                    "RestrictEvents.kext", self.constants.restrictevents_version, self.constants.restrictevents_path
-                )
+            support.BuildSupport(self.model, self.constants, self.config).enable_kext(
+                "RestrictEvents.kext", self.constants.restrictevents_version, self.constants.restrictevents_path
+            )
             self._set_nvram_value(OCLP_UUID, "revblock", block_args, overwrite=True)
 
         if block_args and not patch_args:
@@ -134,14 +129,9 @@ class BuildMiscellaneous:
 
         if patch_args:
             logging.info(f"- Setting RestrictEvents patch arguments: {patch_args}")
-            if self._is_t2_mac():
-                support.BuildSupport(self.model, self.constants, self.config).enable_kext(
-                    "RestrictEvents.kext", self.constants.restrictevents_t2_version, self.constants.restrictevents_t2_path
-                )
-            else:
-                support.BuildSupport(self.model, self.constants, self.config).enable_kext(
-                    "RestrictEvents.kext", self.constants.restrictevents_version, self.constants.restrictevents_path
-                )
+            support.BuildSupport(self.model, self.constants, self.config).enable_kext(
+                "RestrictEvents.kext", self.constants.restrictevents_version, self.constants.restrictevents_path
+            )
             self._set_nvram_value(OCLP_UUID, "revpatch", patch_args, overwrite=True)
 
         kext_obj = support.BuildSupport(self.model, self.constants, self.config).get_kext_by_bundle_path("RestrictEvents.kext")
