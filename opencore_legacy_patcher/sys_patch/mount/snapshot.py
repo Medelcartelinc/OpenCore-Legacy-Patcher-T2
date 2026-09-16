fabc2a856d45ad76368850c1d01d9da2e70f1398
Medelcartelinc <matteoiaccarino97@icloud.com>
2026-09-16 19:55:52 +0200

    Fix bless --bootefi missing on Sequoia x86_64

--- opencore_legacy_patcher/sys_patch/mount/snapshot.py
+++ opencore_legacy_patcher/sys_patch/mount/snapshot.py
@@ -36,8 +36,10 @@ class APFSSnapshot:
             return True
 
         args = ["/usr/sbin/bless"]
-        if platform.machine() == "arm64" or self._rosetta_status() is True or self.xnu_major >= os_data.os_data.sequoia.value:
+        if platform.machine() == "arm64" or self._rosetta_status() is True:
             args += ["--mount", self.mount_path, "--create-snapshot"]
+        elif self.xnu_major >= os_data.os_data.sequoia.value:
+            args += ["--mount", self.mount_path, "--bootefi", "--create-snapshot"]
         else:
             args += ["--folder", f"{self.mount_path}/System/Library/CoreServices", "--bootefi", "--create-snapshot"]
 
